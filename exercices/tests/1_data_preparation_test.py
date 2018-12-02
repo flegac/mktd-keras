f6f985cda259246d8ecda8ca8a49dfe79a3f7899
flegac <florent.legac@gmail.com>
2018-12-02 13:41:07 +0100

    feat: means & std deviation

--- exercices/tests/1_data_preparation_test.py
+++ exercices/tests/1_data_preparation_test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn import preprocessing
 
 
 # - 1.1 pandas : Load a dataset from CSV file using pandas library
@@ -11,19 +12,32 @@ import numpy as np
 
 
 def test_pandas_load_csv():
-    # load csv file
-    data = pd.read_csv('../resources/dataset.csv')
-    expected = (4, 3)
+    expected_shape = (4, 3)
 
-    assert data.shape == expected
+    # load csv file
+    data = None
+    # data = pd.read_csv('../resources/dataset.csv')
+
+    assert data.shape == expected_shape
 
 
 def test_reshape_array():
-    expected = (4, 3, 5)
+    expected_shape = (4, 3, 5)
 
     before = np.array(range(4 * 3 * 5))
+    after = before
 
     #  reshape to shape (4,3,5)
-    after = None
+    # after = np.reshape(before, expected_shape)
 
-    assert after.shape == expected
+    assert after.shape == expected_shape
+
+
+def compute_std_dev_and_mean():
+    data = np.array([[1, 3, 5, 2, 1, 1, 4, 3, 7, 9, 12, 3, 2, 1, 3],
+                     [1, 3, 5, 2, 1, 1, 4, 3, 7, 9, 12, 3, 2, 1, 3]],
+                    dtype=np.float)
+    data_scaled = preprocessing.scale(data)
+
+    data_scaled.mean()
+    data_scaled.std()
