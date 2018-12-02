c034397b91afd7f8cded24a6bb859d877f4c55a3
flegac <florent.legac@gmail.com>
2018-12-02 19:39:16 +0100

    feat: prepare model definitions exercices

---
+++ exercices/tests/2_model_definition.py
@@ -0,0 +1,19 @@
+# - 2.1 Load existing model using h5 files
+# - 2.2 Print summary (layers) of a model
+# - 2.3 Create sequential model, add some layers
+# - 2.4 Save model as h5 file
+
+def load_existing_model():
+    pass
+
+
+def print_model_summary():
+    pass
+
+
+def create_sequential_model():
+    pass
+
+
+def save_model_as_h5():
+    pass
