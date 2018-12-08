76d9b993c34f98dcea3545b1485a87432296a9a8
flegac <florent.legac@gmail.com>
2018-12-08 09:09:14 +0100

    feat: model saving + loading

--- exercices/tests/2_model_definition.py
+++ exercices/tests/2_model_definition.py
@@ -2,27 +2,22 @@
 # - 2.2 Print summary (layers) of a model
 # - 2.3 Create sequential model, add some layers
 # - 2.4 Save model as h5 file
-
+import os
 
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
 
 
-def load_existing_model():
+def load_model(path):
     # load json and create model
-    json_file = open('model.json', 'r')
+    json_file = open(os.path.join(path, 'model.json'), 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     # load weights into new model
-    loaded_model.load_weights("model.h5")
-    print("Loaded model from disk")
-
-
-def print_model_summary():
-    model = None
-    model.summary()
+    loaded_model.load_weights(os.path.join(path, 'model.h5'))
+    return loaded_model
 
 
 def create_sequential_model():
@@ -30,14 +25,28 @@ def create_sequential_model():
     model.add(Dense(12, input_dim=8, kernel_initializer='uniform', activation='relu'))
     model.add(Dense(8, kernel_initializer='uniform', activation='relu'))
     model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
+    return model
 
 
-def save_model_as_h5():
-    model = None
+def save_model(model, path):
     # serialize model to JSON
     model_json = model.to_json()
-    with open("model.json", "w") as json_file:
+    with open(os.path.join(path, 'model.json'), "w") as json_file:
         json_file.write(model_json)
     # serialize weights to HDF5
-    model.save_weights("model.h5")
+    model.save_weights(os.path.join(path, 'model.h5'))
     print("Saved model to disk")
+
+
+def print_model_summary(model):
+    model.summary()
+
+
+path = '.'
+print(os.path.curdir)
+model = create_sequential_model()
+save_model(model, path)
+print_model_summary(model)
+
+model2 = load_model(path)
+print_model_summary(model2)
