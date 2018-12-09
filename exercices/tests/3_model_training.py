f8f3e500dbd35f63035f2a6d4d53297a5458f2e3
flegac <florent.legac@gmail.com>
2018-12-09 19:44:38 +0100

    feat: model training ok

--- exercices/tests/3_model_training.py
+++ exercices/tests/3_model_training.py
@@ -7,16 +7,29 @@
 # 3.7 ImageDataGenerator : data
 # 3.8 model.fit_generator()
 from keras import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, BatchNormalization, Reshape
+from keras.utils import to_categorical
 
 from exercices import utils
 
 
-def create_sequential_model():
-    model = Sequential()
-    model.add(Dense(12, input_dim=8, kernel_initializer='uniform', activation='relu'))
-    model.add(Dense(8, kernel_initializer='uniform', activation='relu'))
-    model.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
+def create_sequential_model(input_shape=(28, 28, 1), output=10):
+    model = Sequential([
+        Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu', input_shape=input_shape),
+        Conv2D(32, kernel_size=(3, 3), padding='same', activation='relu'),
+        MaxPooling2D(),
+        BatchNormalization(),
+
+        Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu'),
+        Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu'),
+        MaxPooling2D(),
+        BatchNormalization(),
+
+        Flatten(),
+        BatchNormalization(),
+        Dense(units=64, activation='relu'),
+        Dense(units=output, activation='softmax')
+    ])
     return model
 
 
@@ -24,14 +37,18 @@ def model_training():
     model = create_sequential_model()
 
     (x_train, y_train), (x_test, y_test) = utils.get_mnist()
+    x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+    x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
 
     # Compile model
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     # Fit the model
-    model.fit(X, Y, epochs=150, batch_size=10, verbose=0)
+    model.fit(x_train, y_train, epochs=20, batch_size=32, verbose=1)
     # evaluate the model
-    scores = model.evaluate(X, Y, verbose=0)
+    scores = model.evaluate(x_test, y_test, verbose=0)
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
 
 
-
+model_training()
